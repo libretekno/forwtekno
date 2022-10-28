1579d0823b122122d3cc4afda8329b1ef9ef90ab
Mehavoid <tontole@riseup.net>
2022-10-29 02:38:25 +0300

    Add render environment

--- forwtekno/__init__.py
+++ forwtekno/__init__.py
@@ -37,13 +37,19 @@ try:
     FROM_CHANNEL = os.environ['FROM_CHANNEL']
     TO_CAPTION = os.environ['TO_CAPTION']
     FROM_CAPTION = os.environ['FROM_CAPTION']
-    HEROKU_APP_NAME = os.environ['APP_NAME']
     PORT = int(os.environ['PORT'])
 except KeyError as exc:
     logger.error(f'{ exc } environment variables are missing!')
     raise SystemExit(1)
 
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+if 'RENDER' in os.environ:
+    WEBHOOK_HOST = f'https://{os.environ["RENDER_EXTERNAL_HOSTNAME"]}'
+elif 'DYNO' in os.environ:
+    WEBHOOK_HOST = f'https://{os.environ["APP_NAME"]}.herokuapp.com'
+else:
+    logger.error('No runtime environment found!')
+    raise SystemExit(1)
+
 WEBHOOK_PATH = f'/bot/{API_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 HOST = '0.0.0.0'
