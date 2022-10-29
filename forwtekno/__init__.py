c2acaba3541cd10ac0bb675ed102eb81830b4700
Mehavoid <tontole@riseup.net>
2022-10-30 02:55:46 +0300

    Refactor logger

--- forwtekno/__init__.py
+++ forwtekno/__init__.py
@@ -1,33 +1,14 @@
 import os
 import logging
-import pathlib
 
-logging.basicConfig(level=logging.DEBUG)
-
-formatter = logging.Formatter(
-  '%(asctime)s - [%(levelname)s] - Line №(%(lineno)d) -> %(message)s'
+logging.basicConfig(
+  level=logging.DEBUG,
+  format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d -> %(message)s'
 )
-
-PATH = pathlib.Path(__file__).parent.parent / 'logs' / (__name__ + '.log')
-
-fh = logging.FileHandler(PATH, 'w')
-fh.setFormatter(formatter)
-fh.setLevel(logging.DEBUG)
-
-ch = logging.StreamHandler()
-ch.setFormatter(formatter)
-ch.setLevel(logging.DEBUG)
+logging.getLogger('aiogram').setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-aiogram_logger = logging.getLogger('aiogram')
-aiogram_logger.setLevel(logging.WARNING)
-aiogram_logger.addHandler(ch)
-aiogram_logger.addHandler(fh)
-
 
 ALLOWED_TYPES = ['audio', 'document', 'photo']
 
@@ -39,7 +20,7 @@ try:
     FROM_CAPTION = os.environ['FROM_CAPTION']
     PORT = int(os.environ['PORT'])
 except KeyError as exc:
-    logger.error(f'{ exc } environment variables are missing!')
+    logger.error(f'{exc} environment variables are missing!')
     raise SystemExit(1)
 
 if 'RENDER' in os.environ:
